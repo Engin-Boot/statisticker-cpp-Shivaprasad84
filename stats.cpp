14c6613de41d11b1a2c772b06e2f3c1aff57cd5f
Shivaprasad <Shivaprasad.S@philips.com>
2020-09-07 01:23:18 +0530

    Updated stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,49 @@
 #include "stats.h"
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<double>& sample) {
+    Stats stats_obj;
+    stats_obj.ComputeAverage(sample);
+    stats_obj.ComputeMax(sample);
+    stats_obj.ComputeMin(sample);
+    return stats_obj;
+}
+
+void Stats::ComputeAverage(const std::vector<double>& sample)
+{
+    Stats::average = NAN;
+    if(sample.size() != 0) 
+    {
+        double sum = 0;
+        for(double element : sample)
+        {
+            if(element != NAN)
+                sum += element;
+        }
+        Stats::average = sum/sample.size();
+    }
+}
+
+void Stats::ComputeMax(const std::vector<double>& sample)
+{
+    Stats::max = sample[0];
+    if(sample.size() != 0)
+    {
+        for(double element : sample)
+        {
+            if(element > Stats::max) Stats::max = element;
+        }
+    }   
+}
+
+void Stats::ComputeMin(const std::vector<double>& sample)
+{
+    Stats::min = sample[0];
+    if(sample.size() != 0)
+    {
+        for(double element : sample)
+        {
+            if(element < Stats::min) Stats::min = element;
+        }
+    }   
 }
